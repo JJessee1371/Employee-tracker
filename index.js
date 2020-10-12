1365e7bf03719f312363a6d46574966f2c14c55d
Jon Jessee <tjessee7624@gmail.com>
2020-10-12 13:50:34 -0600

    Completed basic functionality for adding departments, roles, and employees.

--- index.js
+++ index.js
@@ -56,14 +56,60 @@ function addNew() {
                 break;
 
             case 'Add a new role':
+                inquirer.prompt([
+                    {
+                        name: 'title',
+                        type: 'input',
+                        message: 'What is the title of the role to be added?'
+                    },
+                    {
+                        name: 'salary',
+                        type: 'input',
+                        message: 'What is the salary for this role?'
+                    }
+                ])
+                .then((data) => {
+                    connection.query(
+                        'INSERT INTO role SET ?',
+                        {
+                            title: data.title,
+                            salary: data.salary 
+                        },
+                        function(err) {
+                            if(err) throw err;
+                        }
+                    );
+                });
                 break;
 
             case 'Add a new employee':
+                inquirer.prompt([
+                    {
+                        name: 'firstName',
+                        type: 'input',
+                        message: 'What is the employees first name?'
+                    },
+                    {
+                        name: 'lastName',
+                        type: 'input',
+                        message: 'What is the employees last name?'
+                    },
+                ])
+                .then((data) => {
+                    connection.query(
+                        'INSERT INTO employee SET ?',
+                        {
+                            first_name: data.firstName,
+                            last_name: data.lastName,
+                        },
+                        function(err) {
+                            if(err) throw err;
+                        }
+                    );
+                });
                 break;
-        }
+        };
     });
-
-
 };
 
 addNew();
