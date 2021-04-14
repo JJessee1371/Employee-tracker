834d33ee04567a729c251546bdda98cb720fe8b3
Jon Jessee <tjessee7624@gmail.com>
2021-04-14 07:26:42 -0600

    Input new aliases for view.js and included new joins to return more readable data for the user.

--- JS/view.js
+++ JS/view.js
@@ -18,9 +18,9 @@ const connection = mysql.createConnection({
 module.exports = {
     read:
     async function viewData() {  
-        let answers = await inquirer.prompt([
+        let desiredAction = await inquirer.prompt([
             {
-                name: 'choice',
+                name: 'action',
                 type: 'list',
                 choices: [
                     'View all departments',
@@ -30,20 +30,20 @@ module.exports = {
                 ],
                 message: 'Please select the action you would like to perform:'
             }
-        ])
+        ]);
 
-        let choice = answers.choice;
+        let choice = desiredAction.action;
         let result;
 
         switch(choice) {
             case 'View all departments':
-                result = await queryPromise('SELECT name FROM department');
+                result = await queryPromise('SELECT name AS Departments FROM department');
                 console.table(result);
                 break;
 
             case 'View all roles':
                 result = await queryPromise(
-                    `SELECT title, salary, name AS department_name
+                    `SELECT title AS Title, salary AS Salary, name AS Department
                     FROM role AS t1
                     JOIN department AS t2 ON t1.department_id = t2.department_id
                     ORDER BY name`
@@ -54,10 +54,11 @@ module.exports = {
             //JOIN for full employee data including role and manager
             case 'View all employees':
                 result = await queryPromise(
-                    `SELECT employee_id, first_name, last_name, manager_id, title, salary, name AS department_name
+                    `SELECT employee_id AS ID, first_name AS First, last_name AS Last, manager_id AS Manager_ID, title AS Title, salary AS Salary, name AS Department
                     FROM employee AS t1
                     JOIN role AS t2 ON t1.role_id = t2.role_id
-                    JOIN department AS t3 ON t2.department_id = t3.department_id`
+                    JOIN department AS t3 ON t2.department_id = t3.department_id
+                    ORDER BY Last`
                 );
                 console.table(result);
                 break;
@@ -67,21 +68,27 @@ module.exports = {
                     {
                         name: 'first',
                         type: 'input',
-                        message: "What is the managers' first name?"
+                        message: 'What is the managers\' first name?'
                     }, 
                     {
                         name: 'last',
                         type: 'input',
-                        message: "What is the managers' last name?"
+                        message: 'What is the managers\' last name?'
                     }
                 ]);
 
                 //Get the employee ID of the manager
-                let id = await queryPromise('SELECT employee_id FROM employee WHERE first_name =? AND last_name = ?',
+                let managerID = await queryPromise('SELECT employee_id FROM employee WHERE first_name = ? AND last_name = ?',
                 [managerName.first, managerName.last]);
                 //Locate all employees with the retrieved manager ID
-                let employees = await queryPromise ('SELECT * FROM employee WHERE manager_id = ?',
-                [id[0].employee_id]);
+                let employees = await queryPromise (
+                    `SELECT employee_id AS ID, first_name AS First, last_name AS Last, manager_id AS Manager_ID, title AS Title, salary AS Salary, name AS Department
+                    FROM employee AS t1
+                    JOIN role AS t2 ON t1.role_id = t2.role_id
+                    JOIN department AS t3 ON t2.department_id = t3.department_id
+                    WHERE Manager_id = ?
+                    ORDER BY Last`,
+                [managerID[0].employee_id]);
                 console.table(employees);
                 break;
         };
@@ -89,7 +96,7 @@ module.exports = {
 };
 
 connection.connect((err) => {
-    if (err) console.log(err);
+    if(err) console.log(err);
     queryPromise = util.promisify(connection.query).bind(connection);
     closePromise = util.promisify(connection.end).bind(connection);
 });
